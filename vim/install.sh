e814482de57891bd6d4cd2761d909dec8cbb4414
Fardjad Davari <public@fardjad.com>
2019-04-07 15:26:43 +0430

    Install python3 automatically in vim setup script

--- vim/install.sh
+++ vim/install.sh
@@ -10,9 +10,7 @@ else
     fail 'brew must be installed'
 fi
 
-if ! check_command pip3; then
-    user 'make sure python3 is installed before running vim'
-fi
+brew_install python
 
 if ! check_command npm; then
     user 'make sure node.js is installed before running vim'
