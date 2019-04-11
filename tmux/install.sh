1cf70fb59de79d9eee7fe03bb7c96f0a9dcb05fc
Fardjad Davari <public@fardjad.com>
2019-04-11 20:54:47 +0430

    Add tmux

---
+++ tmux/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+source "$(dirname "$0")/../script/bootstrap.bash"
+
+if check_command brew; then
+    brew_install tmux
+else
+    fail 'brew must be installed'
+fi
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    info 'run tmux and press prefix+I to install the plugins'
+fi
