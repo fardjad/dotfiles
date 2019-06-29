e8bb1bc94976953f3f5ca80c3e8bb791acc81df9
Fardjad Davari <public@fardjad.com>
2019-06-29 21:11:08 +0430

    Update rust install script

--- rust/install.sh
+++ rust/install.sh
@@ -4,17 +4,32 @@ set -e
 
 source "$(dirname "$0")/../script/bootstrap.bash"
 
-if check_command brew; then
-    brew_install rustup-init
+if ! [ -s "$HOME/.cargo/env" ]; then
+    if is_mac; then
+        if check_command brew; then
+            brew_install rustup-init
+        else
+            fail 'brew must be installed'
+        fi
+
+        rustup-init --no-modify-path -y
+    else
+        if check_command brew; then
+            brew_install curl
+        else
+            fail 'brew must be installed'
+        fi
+
+        curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
+    fi
 else
-    fail 'brew must be installed'
+    info "$HOME/.cargo/env already exists" 
 fi
 
-rustup-init --no-modify-path -y
 source "$HOME/.cargo/env"
 
-if check_command rustup; then
+if ! check_command rustfmt; then
     rustup component add rustfmt
 else
-    fail 'rustup is not in path!'
+    info 'rustfmt is already installed'
 fi
