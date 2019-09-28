2ab409f9fb300e688562a2923d3b4618fcc35030
Fardjad Davari <public@fardjad.com>
2019-09-28 18:13:29 +0330

    Update Antibody installation script
    Install Antibody with Linuxbrew on Linux

--- antibody/install.sh
+++ antibody/install.sh
@@ -4,20 +4,12 @@ set -e
 
 source "$(dirname "$0")/../script/bootstrap.bash"
 
-if is_mac; then
-    if ! check_command brew; then
-        fail 'brew must be installed'
-    fi
-
-    brew_tap 'getantibody/tap'
-    brew_install antibody
-else
-    if ! check_command antibody; then
-        curl -sL https://git.io/antibody | sh -s
-    else
-        success "antibody is already installed"
-    fi
+if ! check_command brew; then
+    fail 'brew must be installed'
 fi
+
+brew_tap 'getantibody/tap'
+brew_install antibody
 
 rm -rf $(antibody home)
 antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
