cd68a8620eadff7206ccd89be44e05054c3f9347
Fardjad Davari <public@fardjad.com>
2020-01-11 09:10:14 +0330

    Update deps, add Maven and Gradle

--- sdkman/install.sh
+++ sdkman/install.sh
@@ -6,6 +6,8 @@ source "$(dirname "$0")/../script/bootstrap.bash"
 
 if check_command brew; then
     brew_install curl
+    brew_install zip
+    brew_install unzip
 else
     fail 'brew must be installed'
 fi
@@ -22,15 +24,23 @@ if ! check_command "sdkman"; then
     source "$SDKMAN_DIR/bin/sdkman-init.sh"
 fi
 
-JAVA_VERSION="8.0.222.hs-adpt"
-sdk install java $JAVA_VERSION
+JAVA_VERSION="8.0.232.hs-adpt"
+sdk install java $JAVA_VERSION || true
 sdk default java $JAVA_VERSION
 
-SCALA_VERSION="2.13.0"
-sdk install scala $SCALA_VERSION
+SCALA_VERSION="2.13.1"
+sdk install scala $SCALA_VERSION || true
 sdk default scala $SCALA_VERSION
 
-SBT_VERSION="1.2.8"
-sdk install sbt $SBT_VERSION
+SBT_VERSION="1.3.6"
+sdk install sbt $SBT_VERSION || true
 sdk default sbt $SBT_VERSION
+
+MAVEN_VERSION="3.6.3"
+sdk install maven $MAVEN_VERSION || true
+sdk default maven $MAVEN_VERSION
+
+GRADLE_VERSION="6.0.1"
+sdk install gradle $GRADLE_VERSION || true
+sdk default gradle $GRADLE_VERSION
 
