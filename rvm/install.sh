010f0e13a9ed624b3f2c6c97cc09bc1008b1199a
Fardjad Davari <public@fardjad.com>
2020-10-01 12:15:56 +0330

    rvm: make the installer compatible with macOS

--- rvm/install.sh
+++ rvm/install.sh
@@ -4,16 +4,25 @@ set -e
 
 source "$(dirname "$0")/../script/bootstrap.bash"
 
-if ! check_command gpg2; then
-  user 'gpg2 is not in path. install gnupg2 using your distro package manager and run the installer script again.'
+function install_keys() {
+  GPG_COMMAND="gpg2"
+
+  if is_mac; then
+    brew_install gnupg2
+    GPG_COMMAND="gpg"
+  elif ! check_command gpg2; then
+    user 'gpg2 is not in path. install gnupg2 using your distro package manager and run the installer script again.'
     exit 0
-fi
+  fi
+
+  "$GPG_COMMAND" --keyserver hkp://keys.gnupg.net --receive-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+}
 
 if ! check_command rvm; then
+    install_keys
     RVM="$HOME/.rvm"
     [ -d "$RVM" ] && rm -rf "$RVM"
 
-    gpg2 --keyserver hkp://keys.gnupg.net --receive-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     \curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
     source "$RVM/scripts/rvm"
 fi
