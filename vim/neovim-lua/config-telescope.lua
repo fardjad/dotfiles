9376beb26e6fdddfebf04e1f8c0246de5f0edd97
Fardjad Davari <public@fardjad.com>
2021-12-28 00:39:31 +0100

    vim: Register some telescope lsp keymaps

--- vim/neovim-lua/config-telescope.lua
+++ vim/neovim-lua/config-telescope.lua
@@ -7,5 +7,9 @@ vim.cmd([[
   nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
   nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
   nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+  " lsp related commands are prefixed with l
+  nnoremap <leader>lw <cmd>lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<cr>
+  nnoremap <leader>lr <cmd>lua require('telescope.builtin').lsp_references()<cr>
 ]]);
 
