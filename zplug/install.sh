58be4c0bb752ad7402a3010fa579588f5e7b0d31
Fardjad Davari <public@fardjad.com>
2022-04-22 16:20:19 +0200

    zplug: replace antibody with zplug

---
+++ zplug/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+source "$(dirname "$0")/../script/bootstrap.bash"
+
+if ! check_command brew; then
+  fail 'brew must be installed'
+fi
+
+brew_install curl
+brew_install git
+brew_install zsh
+
+ZSH_PATH="$(brew --prefix)/bin/zsh"
+ZPLUG_HOME="$HOME/.zplug"
+
+if [ ! -d "$ZPLUG_HOME" ]; then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | "$ZSH_PATH"
+else
+  info 'zplug is already installed.'
+fi
---
+++ zplug/zplug.zsh
@@ -0,0 +1,23 @@
+export ZPLUG_HOME="$HOME/.zplug"
+source "$ZPLUG_HOME/init.zsh"
+
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+
+zplug "chriskempson/base16-shell", defer:1
+zplug "mafredri/zsh-async", from:"github", use:"async.zsh"
+zplug "plugins/sudo", from:oh-my-zsh
+zplug "sindresorhus/pure", defer:1
+zplug "zdharma-continuum/fast-syntax-highlighting"
+zplug "zpm-zsh/ls"
+zplug "zsh-users/zsh-autosuggestions" # install the required Ruby version should rvm complain about that
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-history-substring-search"
+
+if ! zplug check; then
+  zplug install
+fi
+
+zplug load
+
+base16_eighties
+fast-theme -q HOME:overlay
