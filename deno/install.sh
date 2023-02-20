7c1c1c218b50eb67f303de099e5532ee6af0c23b
Fardjad Davari <public@fardjad.com>
2023-02-20 22:25:59 +0100

    deno: update install script

--- deno/install.sh
+++ deno/install.sh
@@ -6,12 +6,13 @@ source "$(dirname "$0")/../script/bootstrap.bash"
 
 if check_command brew; then
   brew_install unzip
+  brew_install curl
 else
   fail 'brew must be installed'
 fi
 
 if ! check_command "deno"; then
-  $(which sh) -c "$(curl -fsSL https://deno.land/x/install/install.sh)"
+  curl -fsSL https://deno.land/install.sh | sh
 else
   info 'deno is already intalled. Run "deno upgrade" to upgrade its executable'
 fi
