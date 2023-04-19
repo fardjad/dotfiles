d558efa96165d9cfd4b1cc68c8dcad9675439436
Fardjad Davari <public@fardjad.com>
2023-04-19 14:24:20 +0200

    brew: set environment variables on macOS

--- brew/path.zsh
+++ brew/path.zsh
@@ -1,3 +1,4 @@
 [ -d /home/linuxbrew/.linuxbrew ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+[ -f /opt/homebrew/bin/brew ] && eval $(/opt/homebrew/bin/brew shellenv)
 
 export PATH="$(brew --prefix)/sbin:$PATH"
