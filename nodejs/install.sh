0ca0419d3b66eaa842d18c098f9b53f27e47c93c
Fardjad Davari <public@fardjad.com>
2023-04-27 16:58:04 +0200

    nodejs: fix installer script
    Prevent the installer script from touching ~/.zshrc

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -12,6 +12,7 @@ brew_install git
 
 export NVS_HOME="$HOME/.nvs"
 if [ ! -d "$NVS_HOME" ]; then
+  export NVS_SHELL_PROFILE=/dev/null
   git clone https://github.com/jasongin/nvs "$NVS_HOME"
   . "$NVS_HOME/nvs.sh" install
 else
