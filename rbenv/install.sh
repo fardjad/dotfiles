d946f0008eb41311b7b779959aaa2c4269aa8952
Fardjad Davari <public@fardjad.com>
2023-04-28 19:29:49 +0200

    script: add support for codespaces

--- rbenv/install.sh
+++ rbenv/install.sh
@@ -8,6 +8,11 @@ source "$(dirname "$0")/../script/bootstrap.bash"
 brew_install rbenv
 brew_install ruby-build
 
+if is_codespaces; then
+  # TODO: figure out how to install ruby in codespaces
+  exit 0
+fi
+
 VERSION="$(rbenv install -l | grep -v '-' | sort --version-sort -r | head -n1)"
 eval "$(rbenv init - bash)"
 rbenv install -s "$VERSION"
--- zplug/install.sh
+++ zplug/install.sh
@@ -13,12 +13,17 @@ brew_install git
 brew_install zsh
 
 ZSH_PATH="$(brew --prefix)/bin/zsh"
-ZPLUG_HOME="$HOME/.zplug"
 
-if [ ! -d "$ZPLUG_HOME" ]; then
-  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | "$ZSH_PATH"
+if is_codespaces; then
+  brew_install zplug
 else
-  info 'zplug is already installed.'
-fi
+  ZPLUG_HOME="$HOME/.zplug"
 
-user "run 'rm $ZPLUG_HOME/repos/zsh-users/zsh-autosuggestions/.ruby-version' to remove the ruby version nag message on shell startup. Alternatively, install the missing ruby version with rvm"
+  if [ ! -d "$ZPLUG_HOME" ]; then
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | "$ZSH_PATH"
+  else
+    info 'zplug is already installed.'
+  fi
+
+  user "run 'rm $ZPLUG_HOME/repos/zsh-users/zsh-autosuggestions/.ruby-version' to remove the ruby version nag message on shell startup. Alternatively, install the missing ruby version with rvm"
+fi
