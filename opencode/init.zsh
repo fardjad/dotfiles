11767f88a9a724ad719a621392dff60df1a7f57c
Fardjad Davari <public@fardjad.com>
2026-07-20 02:09:43 +0200

    opencode: add ponytail plugin

--- opencode/init.zsh
+++ opencode/init.zsh
@@ -1,3 +1,5 @@
 # >>> oh-my-opencode-slim background subagents >>>
 export OPENCODE_EXPERIMENTAL_BACKGROUND_SUBAGENTS=true
 # <<< oh-my-opencode-slim background subagents <<<
+
+export PONYTAIL_DEFAULT_MODE="full"
