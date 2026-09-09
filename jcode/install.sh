3e19793780e772417b37f36665c3b6e5e1ab3c1a
Fardjad Davari <public@fardjad.com>
2026-09-10 01:20:18 +0200

    jcode: update config

--- jcode/install.sh
+++ jcode/install.sh
@@ -27,7 +27,6 @@ fi
 mkdir -p "$HOME/.jcode"
 link_file "./jcode.symlink/config.toml" "$HOME/.jcode/config.toml"
 link_file "./jcode.symlink/prompt-overlay.md" "$HOME/.jcode/prompt-overlay.md"
-link_file "./jcode.symlink/swarm-prompt.md" "$HOME/.jcode/swarm-prompt.md"
 
 mkdir -p "$HOME/.jcode/plugins"
 install -m 755 "$JCODE_SOURCE_DIR/plugins/rtk/rtk-transform" \
